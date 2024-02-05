1d1baf0ea794e7381868dd5c8d1262b7b2c809f3
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2024-02-05 12:48:52 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FechaEjercicio3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random azar = new Random();
+            int anyos = azar.Next(0, 4+1);
+
+            DateTime dateTime = DateTime.Now;
+
+            DateTime dateTime2 = dateTime.AddDays(1).AddMonths(24).AddYears(anyos);
+
+            Console.WriteLine(dateTime2.ToString());
+            
+        }
+
+    }
+
+}
